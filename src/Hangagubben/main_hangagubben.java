6c423a49c565800f270194a8fe7c5f1e66bc32db
erikwirenhed <erik.wirenhed.te16d@gmail.com>
2018-02-26 10:05:26 +0100

    Detta är programet som ska köra hänga gubben

--- src/Hangagubben/main_hangagubben.java
+++ src/Hangagubben/main_hangagubben.java
@@ -1,5 +1,22 @@
 package Hangagubben;
 
-public class main_hangagubben {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class main_hangagubben {
+	public static void main(String[] args) throws FileNotFoundException {
+		
+	
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.println("Lätt eller svår?");
+	
+	if(sc.nextLine().equals("lätt")){
+		hangagubben_latt nyttSpel = new hangagubben_latt(new File("Lätta_ord.txt"));
+	}
+	else{
+		
+	}
+}
 }
