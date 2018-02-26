8e7729d6c75bf6d4ee816b909f3c0dd3e72954fb
erikvansvik <erik.vansvik.nti@gmail.com>
2018-02-26 10:45:05 +0100

    Skriver ut en beskrivning på spelet och går vidare till att välja svårhetsgrad

--- src/Hangagubben/main_hangagubben.java
+++ src/Hangagubben/main_hangagubben.java
@@ -10,7 +10,9 @@ public class main_hangagubben {
 	
 	Scanner sc = new Scanner(System.in);
 	
-	System.out.println("Lätt eller svår?");
+	System.out.println("Gissa bokstäverna på det hemliga ordet, men akta för om du gissar fel för många gånger så blir gubben häng och då är spelet över. Välj nu svårhetsgrad.");
+	
+	System.out.println("lätt eller svår?");
 	
 	if(sc.nextLine().equals("lätt")){
 		hangagubben_latt nyttSpel = new hangagubben_latt(new File("Lätta_ord.txt"));
