19f9ed788aceeaa843b2f974cd06bd34b70fe3a5
erikwirenhed <erik.wirenhed.te16d@gmail.com>
2018-02-26 10:01:33 +0100

    jag gjorde en klass som ska ta in en text fil med olika lätta ord. Den ska söka igenom textfilen och ta ett ord som ska användas.

--- src/Hangagubben/hangagubben_latt.java
+++ src/Hangagubben/hangagubben_latt.java
@@ -1,5 +1,23 @@
 package Hangagubben;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class hangagubben_latt {
+	ArrayList<Ord> lättaOrd = new ArrayList<Ord>();
 		
+	public hangagubben_latt(File F) throws FileNotFoundException{
+			Scanner filereader = new Scanner(F);
+			while (filereader.hasNextLine()) {
+
+				String line = filereader.nextLine();
+
+				Ord ord = new Ord(line);
+
+				lättaOrd.add(ord);
+		}
+			filereader.close();
+	}
 }
